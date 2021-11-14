b13ce64834329f1d38b438af17a6199c582243b9
Mayara Felix <mayaralcabral@gmail.com>
2021-11-14 16:12:39 -0500

    Address lint errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,7 +30,10 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new super user"""
-        user = get_user_model().objects.create_superuser('test@gmail.com', 'test123')
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
